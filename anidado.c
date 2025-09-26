d6797176bdaf051cbcc3574e41bca77a851a7d72
jonathanmoreno6373-cell <jonathan.moreno6373@alumnos.udg.mx>
2025-09-26 09:33:46 -0600

    Add files via upload

---
+++ anidado.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int x, op;
+  int j, i;
+
+  for(int i=1; i<=3; i++)
+  {
+      for(int j=1; j<=2; j++)
+      {
+          printf("i=%d, j=%d\n", i, j);
+      }
+  }
+        return 0;
+
+
+
+   /* int op, x;
+    printf("Escriba un numero \n");
+    scanf("%i", &x);
+    printf("Escriba una opcion \n");
+    scanf("%i", &op);
+    switch(op){
+case 1:
+    if(x>0)
+        printf("Positivo");
+    else
+        printf("No positivo");
+    break;
+case 2:
+    if(x%2)
+        printf("ES par");
+    else
+        printf("No es par");
+    break;
+default:
+    printf("no se eligio nada");
+    break;
+    }
+
+
+    return 0;*/
+}
+   /*int edad;
+    printf("Ingresa la edad\n");
+    scanf("%i", & edad);
+   if(edad >=18)
+    {
+       if(edad >=65)
+        printf("adulto es mayor");
+        else{printf("Es mayor");}
+    }
+    else
+    {
+        printf("es menor");
+    }
+
+
+
+    return 0;*/
+
